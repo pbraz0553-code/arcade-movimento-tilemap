dbb101580edc2019a092dd133d6ecab9eab7f482
pbraz0553-code <pbraz0553@gmail.com>
2026-01-19 12:29:29 -0300

    Update menu.ts

--- menu.ts
+++ menu.ts
@@ -1,8 +1,5 @@
 namespace misticoMenu {
 
-    /**
-     * Menu inicial pronto (Play)
-     */
     //% block="criar menu inicial | título %titulo | fundo %fundo | personagem %personagem"
     //% group="Menu"
     export function criarMenu(
@@ -11,33 +8,24 @@ namespace misticoMenu {
         personagem: Sprite
     ) {
 
-        // Cria uma cena separada
         game.pushScene()
         scene.setBackgroundColor(fundo)
 
-        // Título do jogo
         let title = textsprite.create(titulo)
-        title.setMaxFontHeight(16)
         title.setPosition(80, 20)
 
-        // Personagem decorativo
         personagem.setPosition(80, 60)
         personagem.setFlag(SpriteFlag.Ghost, true)
 
-        // Texto PLAY
         let play = textsprite.create("Play")
         play.setPosition(80, 100)
 
-        // Cursor
         let cursor = textsprite.create("▶")
         cursor.setPosition(45, 100)
 
-        // Pressionar A para começar
         controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-            cursor.startEffect(effects.confetti, 200)
-
             timer.after(150, function () {
-                game.popScene() // sai do menu e inicia o jogo
+                game.popScene()
             })
         })
     }
